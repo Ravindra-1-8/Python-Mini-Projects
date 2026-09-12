8abffff6cb2059edafc52f829a250f0a9ccad45e
Ravindra-1-8 <rr7937150@gmail.com>
2026-09-12 17:59:47 +0530

    Electricitybillcalci.py

---
+++ Electricity bill calci.py
@@ -0,0 +1,10 @@
+# Electricity Bill Generator.
+# 100 below units = 5 per unit.
+# 101 - 200 units = 7 per unit.
+# 201 - 300 units = 10 per unit.
+# 300 above units = 15 per unit.
+
+# Example:
+
+# 78 units = 78 * 5
+# But when 105 units = (100 * 5), remaining (5 * 7)
