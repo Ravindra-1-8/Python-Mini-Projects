d00b5f0684b371187ea7142acc1b9989f065f16d
Ravindra-1-8 <rr7937150@gmail.com>
2026-09-12 18:30:33 +0530

    Electricitybillcalci.py

--- Electricity bill calci.py
+++ Electricity bill calci.py
@@ -8,3 +8,20 @@
 
 # 78 units = 78 * 5
 # But when 105 units = (100 * 5), remaining (5 * 7)
+
+def bill_calci(units):
+
+    if units <= 100:
+        bill = units * 5
+    elif units <= 200:
+        bill = (100 * 5) + ((units - 100) * 7)
+    elif units <= 300:
+        bill = (100 * 5) + (100 * 7) + ((units - 200) * 10)
+    else:
+        bill = (100 * 5) + (100 * 7) + (100 * 10) + ((units - 300) * 15)
+    return bill
+
+units = float(input("Enter units: "))
+total_bill = bill_calci(units)
+
+print(f"Total bill: {units} units = {total_bill}/-")
