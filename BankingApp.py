8d1553e71a393244b401a4187bf1c5c4bf090522
Ravindra-1-8 <rr7937150@gmail.com>
2026-09-19 17:07:47 +0530

    BankingApp.py

---
+++ BankingApp.py
@@ -0,0 +1,12 @@
+# PyBank - Simple Banking App.
+
+# Features:
+
+# 1. Deposit money (supports decimal amounts)
+# 2. Withdraw money (cannot exceed available balance)
+# 3. Check current balance.
+# 4. View transaction history.
+# 5. Show total deposits and withdrawal.
+# 6. Menu-based navigation with confirmation messages.
+# 7. Exit option to close the program.
+
