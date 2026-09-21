989f192512a9df25d7c912a531905bc266df9ca7
Ravindra-1-8 <rr7937150@gmail.com>
2026-09-21 18:06:09 +0530

    QuizzApp.py

---
+++ Quiz App.py
@@ -0,0 +1,10 @@
+# Features of Quiz Masters:
+
+# 1. Welcome Screen with App name and instructions.
+# 2. Predefined set of multiple choice questions.
+# 3. User answers with A, B, C, D
+# 4. Correct and wring answer checking.
+# 5. Score calculations.
+# 6. Display total score and percentage.
+# 7. Performance feedback (Excellent / Good/ Practise score).
+# 8. Option to play again (replay feature).
